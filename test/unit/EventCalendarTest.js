d0196ae05a16570ebba3a0f360b2c31ae17458e7
Paul Stanton <paul.stanton@jadu.co.uk>
2018-12-17 10:44:33 +0000

    Testing WIP

--- test/unit/EventCalendarTest.js
+++ test/unit/EventCalendarTest.js
@@ -7,10 +7,11 @@ var moment = require('moment');
 
 describe('EventCalendar', () => {
     const clickEvent = $.Event('click');
-    let $html;
-    let $body;
-    let eventCalendar;
-    let eventCalendarWithoutHTML;
+    let $html,
+        $body,
+        eventCalendar,
+        eventCalendarWithoutHTML,
+        customStartDate = '1981-07-04';
 
     beforeEach(() => {
         $html = $('<div></div>');
@@ -23,7 +24,12 @@ describe('EventCalendar', () => {
         $body.empty();
     });
 
+
     describe('init()', () => {
+        beforeEach(() => {
+            eventCalendar.init();
+        });
+    
 		it('should throw an error if $html isn’t passed to the component', () => {
 			eventCalendarWithoutHTML = new EventCalendar(undefined);
 
@@ -33,23 +39,136 @@ describe('EventCalendar', () => {
         });
 
         it('should throw an error if .js-event-calendar container isn’t present', () => {
-            let $htmlWithoutContainer = $('<div></div>');
-
-            let eventCalendarWithoutContainer = new EventCalendar($htmlWithoutContainer);
+            let $htmlWithoutContainer = $('<div></div>'),
+                eventCalendarWithoutContainer = new EventCalendar($htmlWithoutContainer);
             
 			expect(() => {
                 eventCalendarWithoutContainer.init();
             }).to.throw('EventCalendar requires a .js-event-calendar element present in the DOM');
         });
 
-        it('should default to the current date if no startDate is passed', () => {
-            
+    });
 
-            let today = moment(new Date());
-            console.log(today);
-            console.log(eventCalendar);
 
-            expect(eventCalendar.options.startWithMonth.month).to.equal(today);
+    describe('when no start date is provided', () => {
+        beforeEach(() => {
+            eventCalendar.init();
+        });
+
+        it('should select the current date (today)', () => {
+            let today = moment(new Date()).format('YYYY-MM-DD'),
+                selectedDate = $html.find('.selected > .day-contents').data('day');
+
+            expect(selectedDate).to.equal(today);
+        });
+
+        it('should not set the current date to inactive', () => {
+            expect($html.find('.selected').hasClass('inactive')).to.be.false;
+        });
+
+        it('should constrain the available dates to the day before the startDate', () => {
+            expect($html.find('.selected').prev().hasClass('inactive')).to.be.true;
+        });
+
+        it('should show the month & year in text form (today)', () => {
+            let todayMonthYear = moment(new Date()).format('MMMM YYYY');
+
+            expect($html.find('.month').text()).to.equal(todayMonthYear);
+        });
+
+        it('should not show the previous month arrow', () => {
+            expect($html.find('.clndr-previous-button').hasClass('inactive')).to.be.true;
+        });
+
+        it('should show the next month arrow', () => {
+            expect($html.find('.clndr-next-button').hasClass('inactive')).to.be.false;
+        });
+    });
+
+
+    describe('when a start date is provided', () => {
+        beforeEach(() => {
+            eventCalendar.init(customStartDate);
+        });
+
+        it('should select the specified date', () => {
+            let selectedDate = $html.find('.selected > .day-contents').data('day');
+
+            expect(selectedDate).to.equal(customStartDate);
+        });
+
+        it('should not set the current date to inactive', () => {
+            expect($html.find('.selected').hasClass('inactive')).to.be.false;
+        });
+
+        it('should constrain the available dates to the day before the startDate', () => {
+            expect($html.find('.selected').prev().hasClass('inactive')).to.be.true;
+        });
+
+        it('should show the month & year in text form (start month)', () => {
+            let todayMonthYear = moment(customStartDate).format('MMMM YYYY');
+
+            expect($html.find('.month').text()).to.equal(todayMonthYear);
+        });
+
+        it('should not show the previous month arrow', () => {
+            expect($html.find('.clndr-previous-button').hasClass('inactive')).to.be.true;
+        });
+
+        it('should show the next month arrow', () => {
+            expect($html.find('.clndr-next-button').hasClass('inactive')).to.be.false;
+        });
+    });
+
+
+    describe('when an end date is provided', () => {
+        beforeEach(() => {
+            eventCalendar.init('2018-01-05', '2018-01-10');
+        });
+
+        it('should select the specified date', () => {
+            let selectedDate = $html.find('.selected > .day-contents').data('day');
+
+            expect(selectedDate).to.equal('2018-01-05');
+        });
+
+        it('should not set the current date to inactive', () => {
+            expect($html.find('.selected').hasClass('inactive')).to.be.false;
+        });
+
+        it('should constrain the available dates to the day before the startDate', () => {
+            expect($html.find('.selected').prev().hasClass('inactive')).to.be.true;
+        });
+
+        it('should constrain the available dates to the endDate', () => {
+            expect($html.find('[data-day="2018-01-11"]').parent().hasClass('inactive')).to.be.true;
+        });
+
+        it('should show the month & year in text form (start month)', () => {
+            let todayMonthYear = moment('2018-01-01').format('MMMM YYYY');
+
+            expect($html.find('.month').text()).to.equal(todayMonthYear);
+        });
+
+        it('should not show the previous month arrow', () => {
+            expect($html.find('.clndr-previous-button').hasClass('inactive')).to.be.true;
+        });
+
+        it('should not show the next month arrow', () => {
+            expect($html.find('.clndr-next-button').hasClass('inactive')).to.be.true;
+        });
+    });
+
+
+    describe('clicking a previously unselected date', () => {
+        beforeEach(() => {
+            eventCalendar.init('2018-07-01');
+
+            $html.find('[data-day="2018-07-04"]').trigger(clickEvent);
+        });
+        
+        it('should add the appropriate styling to the desired date', () => {
+            expect($html.find('[data-day="2018-07-04"]').hasClass('event-add')).to.be.true;
         });
     });
 
