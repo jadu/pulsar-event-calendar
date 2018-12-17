78d841b826ed07cf27093d81a564bc7cf420c1b3
Paul Stanton <paul@coffeepowered.co.uk>
2018-12-17 16:33:12 +0000

    Unit tests WIP

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const EventCalendar = require('./src/EventCalendar');
 $(function () {
 
     let myEvents = [
-        { date: '2019-07-25' },
-        { date: '2019-07-26' }
+        { date: '2018-01-25' },
+        { date: '2018-01-26' }
     ];
 
     let startDate   = '2018-01-04';
     let endDate     = '2019-01-10';
 
     const clndr = new EventCalendar($('html'));
-    clndr.init(startDate, endDate);
+    clndr.init(startDate, endDate, myEvents);
 
     $('#js-get').on('click', function() {
         clndr.getDates();
