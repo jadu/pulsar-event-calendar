8d94026c1bec5d293aed53fc04701dabb8e44302
Paul Stanton <paul@coffeepowered.co.uk>
2019-03-07 16:46:40 +0000

    Revert "Clean up demo files"
    This reverts commit b1bce7c25c62060556be85bd76348263929f120d.

--- index.js
+++ index.js
@@ -4,12 +4,28 @@ const EventCalendar = require('./src/EventCalendar');
 $(function () {
     const calendar = new EventCalendar($('html'));
 
+    // let toAdd = [
+    //     '2019-02-05',
+    //     '2019-02-06',
+    //     '2019-02-09',
+    //     '2019-02-10',
+    //     '2019-02-11',
+    //     '2019-02-12'
+    // ];
+
+    // let toDel = [
+    //     '2019-02-08',
+    //     '2019-02-15'
+    // ];
+
     /**
      * Simple init() method which will default to startDate = today / endDate = today + 15 years
      */
     calendar.init({
         startDateField: '.js-ercal-start',
         endDateField: '.js-ercal-end'
+        // datesToAdd: toAdd,
+        // datesToDel: toDel
     });
 
     /**
